dd8bbb28e724cb90588b88abc7b6d4afc5941bc6
Nguyen Huu Phuc <nguyenhuuthuc0405@gmail.com>
2024-06-07 18:46:00 +0700

    Hotel Mangement

---
+++ ManageHotelSystem/HotelSystem/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ManageHotel.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NguyenHuuPhucWPF.Repository;
+using NguyenHuuPhucWPF.View;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace NguyenHuuPhucWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public App()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+    }
+
+}
---
+++ ManageHotelSystem/ManageHotel/Repository/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using NguyenHuuPhucWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NguyenHuuPhucWPF.Repository
+{
+    internal interface IRoomRepository
+    {
+        void Add(RoomInformation room);
+        void Update(RoomInformation room);
+        void Delete(int roomId);
+        RoomInformation GetById(int roomId);
+        IEnumerable<RoomInformation> GetAll();
+    }
+}
